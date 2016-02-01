e992ad8485c5213a32bd600b9d0081b0401d3e4f
Jasper Lyons <jasper.lyons@gmail.com>
2016-02-01 15:43:53 +0000

    Another potential fix for postgres issues
    No longer us t.references, instead use t.integer. May need to
    manually add foreign key later

--- db/migrate/20160129172028_create_content_manager_contents.rb
+++ db/migrate/20160129172028_create_content_manager_contents.rb
@@ -3,7 +3,9 @@ class CreateContentManagerContents < ActiveRecord::Migration
     create_table :content_manager_contents do |t|
       t.integer :version
       t.text :data
-      t.references :view, references: :content_manager_views, index: true, foreign_key: true
+      # sqlite to postgres migration issues:
+      # http://stackoverflow.com/questions/32301686/rails-4-2-engine-reference-column-migration-fails-pgundefinedtable-error-in
+      t.integer :view_id, index: true, foreign_key: true
 
       t.timestamps null: false
     end

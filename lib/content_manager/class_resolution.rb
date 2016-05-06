4ac134803b5b069311e76098900e996bf2a0e151
Jasper Lyons <jasper.lyons@gmail.com>
2016-05-06 19:12:27 +0100

    Update error messages to use correct class name

--- lib/content_manager/class_resolution.rb
+++ lib/content_manager/class_resolution.rb
@@ -1,6 +1,5 @@
 module ContentManager
   module ClassResolution
-
     def self.content_class(name)
       content_class_name(name).constantize
     end
@@ -19,7 +18,7 @@ module ContentManager
           cm_class_name.constantize
           return cm_class_name
         rescue NameError
-          raise "Couldn't find constant definition #{name}, it should be in a file called #{name}.rb"
+          raise "Couldn't find constant definition #{class_name}, it should be in a file called #{class_name}.rb"
         end
       end
     end
